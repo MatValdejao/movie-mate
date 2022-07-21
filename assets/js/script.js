5c86fc4001d8e6362298feb1768b4355f11f1753
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-07-20 21:39:05 -0400

    fixed some bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const apiKey = "1c2cc299";
-const apiKey2 = "6EW8DVJ9Y5sGBFMGXfuttZxSHuABvD5A"
+const apiKey2 = "6EW8DVJ9Y5sGBFMGXfuttZxSHuABvD5A";
 var mainContentEl = document.querySelector(".main-content");
 var submitButtonEl = document.getElementById("submit-btn");
 var movieListEl = document.querySelector(".movie-list");
@@ -8,34 +8,41 @@ var searchHistory = [];
 var retrieveData = function (movie) {
 	var apiURL = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movie;
 
-	fetch(apiURL).then(function (res) {
-		if (res.ok) {
-            res.json().then(function (data) {
-                if (data.Response === "True") {
-                    movieReview(data, movie);
-                } 
-                else {
-                    alert(data.Error)
-                }
-			});
-        }
-    }).catch(function (error) {
-        alert("Unable to connect to Movies API")
-    });
+	fetch(apiURL)
+		.then(function (res) {
+			if (res.ok) {
+				res.json().then(function (data) {
+					if (data.Response === "True") {
+						movieReview(data, movie);
+					} else {
+						console.log(data.Error);
+					}
+				});
+			}
+		})
+		.catch(function (error) {
+			console.log("Unable to connect to Movies API");
+		});
 };
 
 var movieReview = function (data2, movie) {
-	var apiURL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" + apiKey2 + "&query=" + movie;
+	var apiURL =
+		"https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" +
+		apiKey2 +
+		"&query=" +
+		movie;
 
-	fetch(apiURL).then(function (res) {
-		if (res.ok) {
-            res.json().then(function (data) {
-                displayMovie(data2, movie, data);
-			});
-        }
-    }).catch(function (error) {
-        alert("Unable to connect to Movies API")
-    });
+	fetch(apiURL)
+		.then(function (res) {
+			if (res.ok) {
+				res.json().then(function (data) {
+					displayMovie(data2, movie, data);
+				});
+			}
+		})
+		.catch(function (error) {
+			console.log("Unable to connect to Movies API");
+		});
 };
 
 var getMovie = function (event) {
@@ -44,12 +51,13 @@ var getMovie = function (event) {
 
 	// retrieves button element text data
 	var movie = inputEl.value.trim();
-
+	movie = movie.toUpperCase();
+	
 	// calls retrieveData function
 	retrieveData(movie);
 
 	// clear input field
-	inputEl.value = ""
+	inputEl.value = "";
 };
 
 var displayMovie = function (data2, movie, data) {
@@ -57,49 +65,49 @@ var displayMovie = function (data2, movie, data) {
 
 	mainContentEl.style.display = "block";
 
-    var reviewData = data
-    console.log(reviewData)
+	var reviewData = data;
+
 	// get movie data
-    var movieData = data2;
-    
-    // calls carousel builder functions
+	var movieData = data2;
+
+	// calls carousel builder functions
 	// scale the headers of the cards down
 	// enlarge the content font slightly to fill the card
-    carouselItemOne(movieData, movie);
-    carouselItemTwo(movieData, movie);
-    carouselItemThree(reviewData, movie);
-    carouselItemFour(movieData, movie);
+	carouselItemOne(movieData, movie);
+	carouselItemTwo(movieData, movie);
+	carouselItemThree(reviewData, movie);
+	carouselItemFour(movieData, movie);
 };
 
-var carouselItemOne = function (movieData, movie) {	
-    // select first carousel item
+var carouselItemOne = function (movieData, movie) {
+	// select first carousel item
 	var carouselOneEl = document.querySelector(".item1");
 
 	// display movie title as first carousel item
 	// move display to the header of the section
 	// add age rating in its place maybe?
 	var movieTitleEl = document.createElement("h3");
-	movieTitleEl.classList = "carousel-val"
+	movieTitleEl.classList = "carousel-val";
 	movieTitleEl.style.color = "black";
 	movieTitleEl.style.textAlign = "center";
 	movieTitleEl.textContent = movie;
-	
+
 	// date movie released, together with movie title carousel
 	var releaseDateEl = document.createElement("h3");
-	releaseDateEl.classList = "carousel-val"
+	releaseDateEl.classList = "carousel-val";
 	releaseDateEl.style.color = "black";
 	releaseDateEl.style.textAlign = "center";
 	var releaseDateSplit = movieData.Released.split(" ");
 	var releaseYear = releaseDateSplit[2];
-    releaseDateEl.textContent = "(" + releaseYear + ")";
-    
-    // append to page
-    carouselOneEl.appendChild(movieTitleEl);
+	releaseDateEl.textContent = "(" + releaseYear + ")";
+
+	// append to page
+	carouselOneEl.appendChild(movieTitleEl);
 	carouselOneEl.appendChild(releaseDateEl);
-}
+};
 
 var carouselItemTwo = function (movieData, movie) {
-    // get carousel element two
+	// get carousel element two
 	var carouselTwoEl = document.querySelector(".item2");
 
 	// plot element carousel item
@@ -112,21 +120,21 @@ var carouselItemTwo = function (movieData, movie) {
 	// plot description
 	var plotEl = document.createElement("p");
 	plotEl.style.color = "black";
-    plotEl.style.textAlign = "center";
-    // checks whether plot description exists
+	plotEl.style.textAlign = "center";
+	// checks whether plot description exists
 	if (movieData.Plot === "N/A") {
 		plotEl.textContent = "Sorry! No Plot Provided!";
 	} else {
 		plotEl.textContent = movieData.Plot;
-    }
-    
-    // append to page
-    carouselTwoEl.appendChild(plotTitleEl);
+	}
+
+	// append to page
+	carouselTwoEl.appendChild(plotTitleEl);
 	carouselTwoEl.appendChild(plotEl);
-}
+};
 
 var carouselItemThree = function (reviewData, movie) {
-    // get carousel element three
+	// get carousel element three
 	var carouselThreeEl = document.querySelector(".item3");
 
 	// reviews section carousel item
@@ -135,20 +143,26 @@ var carouselItemThree = function (reviewData, movie) {
 	reviewEl.style.textAlign = "center";
 	reviewEl.textContent = "Reviews";
 
-	// takes review from other API
+	var movieReviewEl = document.createElement("p");
+	movieReviewEl.style.color = "black";
+	movieReviewEl.style.textAlign = "center";
+	if (reviewData.results === null) {
+		movieReviewEl.textContent = "No review available for " + movie;
+	} else {
+		movieReviewEl.innerHTML =
+			"<a href=" +
+			reviewData.results[0].link.url +
+			" target = _blank> Review To " +
+			movie +
+			"</a>";
+	}
+	movieReviewEl.style.textDecoration = "none";
+	movieReviewEl.style.color = "black";
 
-	 var movieReviewEl = document.createElement("p");
-	 movieReviewEl.style.color = "black";
-	 movieReviewEl.style.textAlign = "center";
-	 movieReviewEl.innerHTML = "<a href=" + reviewData.results[0].link.url + " target = _blank> Review To " + movie + "</a>";
-     movieReviewEl.style.textDecoration = "none"
-     movieReviewEl.style.color = "black"
-     
-
-    // append to page
-    carouselThreeEl.appendChild(reviewEl);
-    carouselThreeEl.appendChild(movieReviewEl);
-}
+	// append to page
+	carouselThreeEl.appendChild(reviewEl);
+	carouselThreeEl.appendChild(movieReviewEl);
+};
 
 var carouselItemFour = function (movieData, movie) {
 	// gets carousel element four
@@ -170,10 +184,10 @@ var carouselItemFour = function (movieData, movie) {
 		listItemEl.textContent = actorsList[i];
 		listItemEl.style.color = "black";
 		actorListEl.appendChild(listItemEl);
-    }
+	}
 
-    // append to page
-    carouselFourEl.appendChild(castEl);
+	// append to page
+	carouselFourEl.appendChild(castEl);
 	carouselFourEl.appendChild(actorListEl);
 
 	// checks whether searched movie earned awards, if so, display awards on screen
@@ -194,30 +208,28 @@ var carouselItemFour = function (movieData, movie) {
 			listItemEl.style.color = "black";
 			awardListEl.appendChild(listItemEl);
 		}
-        // append to page when exists
+		// append to page when exists
 		carouselFourEl.appendChild(awardsEl);
 		carouselFourEl.appendChild(awardListEl);
 	}
-}
+};
 
 document.addEventListener("DOMContentLoaded", function () {
 	var elems = document.querySelectorAll(".carousel");
 	var instances = M.Carousel.init(elems);
-	mainContentEl.style.display = "none"
+	mainContentEl.style.display = "none";
 });
-
 
 // add "Enter" key as possible input
 var input = document.getElementById("search");
-input.addEventListener("keypress", function(event) {
+input.addEventListener("keypress", function (event) {
 	if (event.key === "Enter") {
 		event.preventDefault();
 		getMovie();
 	}
-})
+});
 
 var historyButtons = function (movie) {
-
 	// create history button for specific movie
 	var histButtonEl = document.createElement("button");
 	histButtonEl.setAttribute("type", "submit");
@@ -230,30 +242,29 @@ var historyButtons = function (movie) {
 
 var saveMovie = function (movie) {
 	if (searchHistory.indexOf(movie) === -1) {
-		movie = movie.toUpperCase()
+		movie = movie.toUpperCase();
 		searchHistory.push(movie);
 
 		// save city to page
 		historyButtons(movie);
 	}
 	localStorage.setItem("movie", searchHistory);
-}
+};
 
 var savedStorage = function () {
 	searchHistory = localStorage.getItem("movie");
 
 	// check whether searchHistory exists
 	if (searchHistory === null) {
-		searchHistory = []
-		return
+		searchHistory = [];
+		return;
 	}
 
 	searchHistory = searchHistory.split(",");
 	for (var i = 0; i < searchHistory.length; i++) {
 		historyButtons(searchHistory[i]);
 	}
-}
-
+};
 
 savedStorage();
 
@@ -265,6 +276,3 @@ $(".movie-list").on("click", ".history-btn", function (event) {
 
 	retrieveData(movie);
 });
-
-
-
